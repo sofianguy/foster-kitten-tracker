a301d7b68f223f4e11997b3aa5508b53303cd4d5
Sofia Nguy <sofianguy@gmail.com>
2017-05-16 14:36:15 -0700

    hook up server to render html file

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.sendFile(path.join(__dirname + '/views/home.html'));
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
