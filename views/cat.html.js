24fb8c5711956f3bccc1cf712140102b58998b87
Sofia Nguy <sofianguy@gmail.com>
2017-05-16 17:42:19 -0700

    chart for kitten info; hardcode sample info

--- views/cat.html.js
+++ views/cat.html.js
@@ -7,6 +7,23 @@ exports.cat = (name) => `
 
   <body>
     <h1>I'm ${name}</h1>
+    <table>
+      <tr>
+        <th>date</th>
+        <th>age</th>
+        <th>weight(grams)</th>
+        <th>milk(ml)</th>
+        <th>approx number of feedings</th>
+        <th>notes</th>
+      </tr>
+      <tr>
+        <td>Wednesday, May 10, 2017</td>
+        <td>6 weeks</td>
+        <td>700g</td>
+        <td>n/a</td>
+        <td>3</td>
+      </tr>
+    </table>
   </body>
   </html>
 `
