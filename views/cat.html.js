7a12b63bb01e766cb03df512990ea08971e93a47
Sofia Nguy <sofianguy@gmail.com>
2017-05-17 20:31:36 -0700

    word fixes; form to add growth info; css padding for growth table

--- views/cat.html.js
+++ views/cat.html.js
@@ -7,8 +7,22 @@ exports.cat = (name) => `
   </head>
 
   <body>
+    <!-- INTRODUCTION INFO: -->
     <img src="" alt="cat ${name}">
     <h1>I'm ${name}</h1>
+
+    <!-- FORM TO ADD GROWTH INFO: -->
+    <form action="/cat/${name}">
+      <input type="text" name="date" placeholder="May 17, 2017">
+      <input type="text" name="age" placeholder="6 weeks">
+      <input type="text" name="weight" placeholder="700 g">
+      <input type="text" name="milk" placeholder="20 ml">
+      <input type="text" name="numberFeedings" placeholder="3">
+      <input type="text" name="notes" placeholder="doing great today">
+      <input type="submit" value="add">
+    </form>
+
+    <!-- CHART FOR GROWTH: -->
     <table>
       <tr>
         <th>date</th>
@@ -19,11 +33,12 @@ exports.cat = (name) => `
         <th>notes</th>
       </tr>
       <tr>
-        <td>Wednesday, May 10, 2017</td>
+        <td>May 10, 2017</td>
         <td>6 weeks</td>
         <td>700g</td>
         <td>n/a</td>
         <td>3</td>
+        <td></td>
       </tr>
     </table>
   </body>
