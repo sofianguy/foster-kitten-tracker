d51326733443175db585a639fe91ff05b734273e
Sofia Nguy <sofianguy@gmail.com>
2017-05-29 12:31:40 -0700

    add new kitten button on home page

--- views/home.html.js
+++ views/home.html.js
@@ -34,9 +34,38 @@ exports.home = (cats) => {
         <h1>Track your kitten</h1>
       </div>
       <h3>pick a kitten:</h3>
-      <button type="button" class="btn btn-primary">add a kitten</button>
+
+      <!-- add a kitten modal form -->
+      <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#add-a-kitten">add a kitten</button>
+      <div id="add-a-kitten" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">add a new kitten</h4>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="name">name</label>
+                  <input type="text" class="form-control" name="name" placeholder="fluffy">
+                </div>
+                <div class="form-group">
+                  <label for="image">image</label>
+                  <input type="text" class="form-control" name="image" placeholder="http://content.mycutegraphics.com/graphics/cats/cat-mouse-yarn.png">
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary">add</button>
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
-    
+
     <div class="row">
       ${catLinks}
     </div>
