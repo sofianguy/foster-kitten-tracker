ed0eafd93fa227117d95e99b682b21dad34eb96c
Sofia Nguy <sofianguy@gmail.com>
2020-01-14 19:24:56 -0800

    new photos

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ class CatStorage {
     this.data = {
       cats: [
         {
-          info: {name:'pigwidgeon', path:'/cat/pigwidgeon', image:'http://content.mycutegraphics.com/graphics/cats/cat-mouse-yarn.png'},
+          info: {name:'pigwidgeon', path:'/cat/pigwidgeon', image:'https://i.imgur.com/ea7WXND.jpg'},
           growth: [
             {date:'05/10/2017', age:'6', weight:750, milk: 'n/a', feedings:3, notes:'n/a', medicalNotes:'gave meds'},
             {date:'05/11/2017', age:'6', weight:760, milk: 'n/a', feedings:3, notes:'n/a', medicalNotes:'gave meds'}
           ]
         },
         {
-          info: {name:'errol', path:'/cat/errol', image:'http://content.mycutegraphics.com/graphics/cats/cat-mouse-yarn.png'},
+          info: {name:'errol', path:'/cat/errol', image:'https://i.imgur.com/VcXX9Pv.jpg'},
           growth: [
             {date:'05/10/2017', age:'6', weight:800, milk: 'n/a', feedings:3, notes:'n/a', medicalNotes:'gave meds'},
             {date:'05/11/2017', age:'6', weight:810, milk: 'n/a', feedings:3, notes:'n/a', medicalNotes:'gave meds'}
